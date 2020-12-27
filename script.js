f186f601c00b2114ea49541e8ec583bbc45e4e9d
Luwenxi Song <songl652@newschool.edu>
2020-12-26 20:02:43 -0500

    Fixed unit

--- script.js
+++ script.js
@@ -30,22 +30,23 @@ $(array[i-1]).text(tomorrow)
 
 
 function weatherToday(city){
-  var current = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey
-  var future = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apikey
+  var current = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+apikey
+  var future = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&units=metric&appid="+apikey
 
   $.ajax({
     url: current,
     method: "GET"
   }).then(function(response) {
 
-   
+   console.log(response)
     
     $("#iname").text(response.name)
     $('#mainc').attr("src","http://openweathermap.org/img/wn/"+response.weather[0].icon+"@2x.png")
     $('#mainc').attr("alt",response.weather[0].main)
-    $("#itemp").text("Temperature: "+response.main.temp)
-    $("#ihumi").text("Humidity: "+response.main.humidity)
-    $("#iwind").text("Wind Speed: "+response.wind.speed)
+    $("#itemp").text("Temperature: "+response.main.temp+"℃")
+    $("#ihumi").text("Humidity: "+response.main.humidity+"%")
+    $("#iwind").text("Wind Speed: "+response.wind.speed+"M/s")
+
 }
 )
 $.ajax({
@@ -54,28 +55,28 @@ $.ajax({
 }).then(function(response) {
   $("#amain").attr("src","http://openweathermap.org/img/wn/"+response.list[7].weather[0].icon+"@2x.png")
   //$("#adate").text(response.list[7])
-  $("#atemp").text("Temperature: "+response.list[7].main.temp)
-  $("#ahumi").text("Humidity: "+response.list[7].main.humidity)
+  $("#atemp").text("Temperature: "+response.list[7].main.temp+"℃")
+  $("#ahumi").text("Humidity: "+response.list[7].main.humidity+"%")
 
   $("#bmain").attr("src","http://openweathermap.org/img/wn/"+response.list[14].weather[0].icon+"@2x.png")
   //$("#adate").text(response.list[7])
-  $("#btemp").text("Temperature: "+response.list[14].main.temp)
-  $("#bhumi").text("Humidity: "+response.list[14].main.humidity)
+  $("#btemp").text("Temperature: "+response.list[14].main.temp+"℃")
+  $("#bhumi").text("Humidity: "+response.list[14].main.humidity+"%")
   
   $("#cmain").attr("src","http://openweathermap.org/img/wn/"+response.list[21].weather[0].icon+"@2x.png")
   //$("#adate").text(response.list[7])
-  $("#ctemp").text("Temperature: "+response.list[21].main.temp)
-  $("#chumi").text("Humidity: "+response.list[21].main.humidity)
+  $("#ctemp").text("Temperature: "+response.list[21].main.temp+"℃")
+  $("#chumi").text("Humidity: "+response.list[21].main.humidity+"%")
 
   $("#dmain").attr("src","http://openweathermap.org/img/wn/"+response.list[28].weather[0].icon+"@2x.png")
   //$("#adate").text(response.list[7])
-  $("#dtemp").text("Temperature: "+response.list[28].main.temp)
-  $("#dhumi").text("Humidity: "+response.list[28].main.humidity)
+  $("#dtemp").text("Temperature: "+response.list[28].main.temp+"℃")
+  $("#dhumi").text("Humidity: "+response.list[28].main.humidity+"%")
 
   $("#emain").attr("src","http://openweathermap.org/img/wn/"+response.list[35].weather[0].icon+"@2x.png")
   //$("#adate").text(response.list[7])
-  $("#etemp").text("Temperature: "+response.list[35].main.temp)
-  $("#ehumi").text("Humidity: "+response.list[35].main.humidity)
+  $("#etemp").text("Temperature: "+response.list[35].main.temp+"℃")
+  $("#ehumi").text("Humidity: "+response.list[35].main.humidity+"%")
 
  //24/3=8 taking every 7th item in the array as sample because I don't have the startup subscribtion for daily 16 days forcast :(
 
